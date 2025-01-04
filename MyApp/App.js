eee17c17822995d2dab16e25552c0881f7f79a16
ruwini01 <12345tharanga12345@gmail.com>
2025-01-04 14:13:17 +0530

    Header Bar Updated

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from '../screens/Login';
-import BottomTabNavigator from './BottomTabNavigator';
+import Login from './src/screens/Login';
+import BottomTabNavigator from './src/components/BottomTabNavigator';
+import { Image } from 'react-native';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator screenOptions={{ 
+        headerShown: true, 
+        headerTintColor:'white',
+        headerTitle:'UoV Student Care' ,
+        headerStyle:{
+          backgroundColor:'#4b0150'
+        }
+        }}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="BottomTabs" component={BottomTabNavigator} />
       </Stack.Navigator>
