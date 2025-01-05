44637b81a09c3d8391957f9584bafdc65e22bea6
ruwini01 <12345tharanga12345@gmail.com>
2025-01-05 10:42:24 +0530

    Subjects page connected with StudentsDB

--- MyApp/src/screens/Subjects.js
+++ MyApp/src/screens/Subjects.js
@@ -3,8 +3,30 @@ import { View, Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 import Logo from '../components/Logo';
 import Footer from '../components/Footer';
 import { Divider, DataTable } from 'react-native-paper';
+import { subjects, marks,courses } from '../../assets/data/StudentsDb';
 
-export default function Courses() {
+export default function Courses({route}) {
+
+  const {user} = route.params;
+
+  const userSubjects = subjects.filter(subject => subject.course_id === user.course_id);
+  const userMarks = marks.filter(mark => mark.student_id === user.id);
+  //console.log(userMarks);
+  
+
+  const matchMarks = (subjectId) => {
+    const mark = userMarks.find(m => m.subject_id === subjectId);
+    return mark ? mark.marks : '-';
+  };
+
+
+  const average = () => {
+    let sum = 0;
+    userMarks.forEach((arr) => sum += arr["marks"]);
+    return Math.round(sum/3);
+    
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
@@ -13,33 +35,24 @@ export default function Courses() {
         </View>
         <View style={styles.centerContainer}>
           <View style={styles.card}>
-            <Text style={styles.title}>Computer Science</Text>
-            <Text style={styles.description}>3 Subjects | Average: 88</Text>
+            <Text style={styles.title}>{courses.find(c => c.id === user.course_id).name}</Text>
+            <Text style={styles.description}>{userSubjects.length} Subjects | Average: {average()}</Text>
             <Divider style={{ width: '80%', marginVertical: 20, backgroundColor: '#ccc' }} />
             <View style={styles.marksInfo}>
               <Text style={styles.subtitle}>Marks Information</Text>
-              <DataTable>
+              <DataTable style={{paddingBottom:20}}>
                 <DataTable.Header>
                   <DataTable.Title>Subject</DataTable.Title>
                   <DataTable.Title numeric>Mark</DataTable.Title>
                 </DataTable.Header>
 
-                <DataTable.Row>
-                  <DataTable.Cell>Data Structures</DataTable.Cell>
-                  <DataTable.Cell numeric>90</DataTable.Cell>
-                </DataTable.Row>
-
-                <DataTable.Row>
-                  <DataTable.Cell>Algorithms</DataTable.Cell>
-                  <DataTable.Cell numeric>85</DataTable.Cell>
-                </DataTable.Row>
-
-                <DataTable.Row>
-                  <DataTable.Cell>System Design</DataTable.Cell>
-                  <DataTable.Cell numeric>90</DataTable.Cell>
-                </DataTable.Row>
+                {userSubjects.map(subject => (
+                  <DataTable.Row key={subject.id}>
+                    <DataTable.Cell>{subject.name}</DataTable.Cell>
+                    <DataTable.Cell numeric>{matchMarks(subject.id)}</DataTable.Cell>
+                  </DataTable.Row>
+                ))}
               </DataTable>
-              <Divider style={{ width: '80%', marginVertical: 20, backgroundColor: '#ccc' }} />
             </View>
           </View>
         </View>
@@ -48,8 +61,7 @@ export default function Courses() {
         <Footer />
       </View>
     </SafeAreaView>
-  );
-}
+  );}
 
 const styles = StyleSheet.create({
   container: {
