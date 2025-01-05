09651e02714ae15e7637f0b3d74dafba287b138a
ruwini01 <12345tharanga12345@gmail.com>
2025-01-05 10:41:05 +0530

    Login Page connected with studentDB

--- MyApp/src/screens/Login.js
+++ MyApp/src/screens/Login.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import Logo from '../components/Logo';
 import Footer from '../components/Footer';
 import { TextInput } from 'react-native-paper';
 import { useState } from 'react';
+import { students } from '../../assets/data/StudentsDb';
 
-export default function Login({ navigation }) {
-
+export default function Login({ navigation, setUser }) {
   const [form, setForm] = useState({
-    username:'',
-    password:''
+    username: '',
+    password: ''
   });
   const [showpassword, setShowPassword] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+  const login = () => {
+    const user = students.find(student => student.username === form.username && student.password === form.password);
+    if (user) {
+      setUser(user)
+      navigation.navigate('BottomTabs', { user });
+    } else {
+      setShowErrorMessage(true);
+    }
+  }
 
   return (
     <View style={styles.container}>
-
       <View style={styles.logoContainer}>
         <Logo />
       </View>
@@ -23,35 +33,51 @@ export default function Login({ navigation }) {
       <View style={styles.formContainer}>
         <Text style={styles.title}>STUDENT LOGIN</Text>
         <TextInput
-            label="Email"
-            style={styles.input}
-            mode='outlined'
-            value={form.username}
-            onChangeText={(text) => setForm({
+          label="Username"
+          style={styles.input}
+          mode='outlined'
+          value={form.username}
+          onChangeText={(text) => {
+            setShowErrorMessage(false);
+            setForm({
               ...form,
-              username:text
-            })}
+              username: text
+            });
+          }}
         />
         <TextInput
           label="Password"
           secureTextEntry={!showpassword}
           style={styles.input}
           right={<TextInput.Icon 
-            icon={showpassword? 'eye-off': 'eye'}
+            icon={showpassword ? 'eye-off' : 'eye'}
             onPress={() => setShowPassword(!showpassword)} />}
           mode='outlined'
           value={form.password}
-          onChangeText={(text) => setForm({
-            ...form,
-            password:text
-          })}
-    />
+          onChangeText={(text) => {
+            setShowErrorMessage(false);
+            setForm({
+              ...form,
+              password: text
+            });
+          }}
+        />
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('BottomTabs')}
+          onPress={login}
         >
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
+
+        {showErrorMessage && 
+          <View style={styles.error}>
+            <Image 
+              source={require('../../assets/icons/error.png')} 
+              style={styles.errorIcon}
+            /> 
+            <Text style={styles.errorText}>Please check your username and password</Text>
+          </View>
+        }
       </View>
 
       <View style={styles.footerContainer}>
@@ -85,8 +111,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   title: {
-    marginTop:-50,
-    paddingBottom:100,
+    marginTop: -50,
+    paddingBottom: 40,
     fontSize: 32,
     fontWeight: 'semibold',
     marginBottom: 50,
@@ -104,10 +130,31 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 50,
+    marginBottom: 15,
   },
   buttonText: {
     color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
   },
+  error: {
+    width: '100%',
+    height: 40,
+    flexDirection: 'row',
+    backgroundColor: '#F3E5F5',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    borderRadius: 10,
+  },
+  errorIcon: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+    marginRight: 10,
+  },
+  errorText: {
+    fontSize: 14,
+    fontWeight:600,
+    flex: 1,
+  }
 });
